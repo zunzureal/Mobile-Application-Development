1346f49a0049f9fe427f99d73cf3a50e1dd9f16c
zunzu (Punyapat Chanthakhun) <57216006+zunzureal@users.noreply.github.com>
2023-02-03 09:44:28 +0700

    Add files via upload

---
+++ Excersice2/Assignment1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Assignment1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a, b, c, d, e, f: ");
+    double a = input.nextDouble();
+    double b = input.nextDouble();
+    double c = input.nextDouble();
+    double d = input.nextDouble();
+    double e = input.nextDouble();
+    double f = input.nextDouble();
+    double g = (a*d) - (b*c);
+    double x, y;
+     if (g != 0) {
+     x = ((e*d) - (b*f)) / ((a*d) - (b*c));
+     y = ((a*f) - (e*c)) / ((a*d) - (b*c));
+     System.out.print("x is " + x + " and y is " + y);
+  }
+    else {
+      System.out.print("The equation has no solution");
+    }
+}
+}    
